4c946fb3b87b2886388b0ce19b1fb62c8c03c6bc
Fatemeh Salimi <salimi.fsb@gmail.com>
2023-11-12 11:39:15 +0100

    Unit tests

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,5 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_random_integer, generate_random_operator, calculate_result
 
 
 class TestMathGame(unittest.TestCase):
@@ -9,22 +9,27 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generate_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
-        pass
+        # Test if the generated operator is one of '+', '-', or '*'
+        for _ in range(1000):  # Test a large number of random values
+            rand_operator = generate_random_operator()
+            self.assertIn(rand_operator, ['+', '-', '*'])
 
     def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (3, 8, '-', '3 - 8', -5),
+            (4, 6, '*', '4 * 6', 24)
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            problem, answer = calculate_result(num1, num2, operator)
+            self.assertEqual(problem, expected_problem)
+            self.assertEqual(answer, expected_answer)
+
 
 if __name__ == "__main__":
     unittest.main()
